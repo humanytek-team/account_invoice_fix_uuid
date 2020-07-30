e45699e071e62523a601817cd177b4ad4c53ecac
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2020-07-30 14:27:22 -0500

    [IMP] imports

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import base64
+import importlib
+
 
 from odoo import _, api, fields, models
 
@@ -7,29 +9,8 @@ from odoo import _, api, fields, models
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    def _import(self, module):
+        importlib.import_module(module)
+
     def _to_base64(self, data):
         return base64.b64decode(data)
-
-    def _fix_uuids(self):
-        invoices = self.search([
-            ('number', '!=', False),
-            ('l10n_mx_edi_cfdi_uuid', '=', ''),
-            ('state', '=', 'open'),
-            ('type', '=', 'out_invoice'),
-        ])
-
-        res = ''
-
-        for r in invoices:
-            first_attachment = self.env['ir.attachment'].search(
-                [
-                    ('res_model', '=', self._name),
-                    ('res_id', '=', r.id),
-                ],
-                order='create_date',
-                limit=1,
-            )
-            if first_attachment.datas_fname[-3:] == 'xml':
-                res += '{} {} {}\n'.format(r.number, first_attachment.datas_fname, xml)
-
-        return res
